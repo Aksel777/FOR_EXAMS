9d3a42f1dd24fc45bbcd5b6fc250e4f5edddda03
Ksenia <418875603@mail.ru>
2023-11-22 23:51:38 +0300

    changed a folder

---
+++ Итоговая контрольная работа по основному блоку/ЗАДАНИЕ/Program.cs
@@ -0,0 +1,52 @@
+﻿//  Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых
+// меньше, либо равна 3 символам. Первоначальный массив
+// можно ввести с клавиатуры, либо задать на старте выполнения 
+// алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите количество элементов массива: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] stringArray = new string[n];
+
+        void Array(string[] stringArray)
+        {
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} элемент массива");
+                stringArray[i] = Console.ReadLine()!;
+            }
+        }
+        string[] NewArray(string[] stringArray)
+        {
+            int n = 0;
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (stringArray[i].Length <= 3)
+                    n++;
+            }
+            string[] newArray = new string[n];
+            int j = 0;
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (stringArray[i].Length <= 3)
+                {
+                    newArray[j] = stringArray[i];
+                    j++;
+                }
+            }
+            return newArray;
+        }
+
+        Array(stringArray);
+        Console.Write($"Исходник:[{string.Join(',', stringArray)}]");
+        Console.WriteLine();
+
+        NewArray(stringArray);
+        Console.Write($"Новый массив:[{string.Join(',', NewArray(stringArray))}]");
+    }
+}
